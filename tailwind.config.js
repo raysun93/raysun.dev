bb3a1fe6db08516f1b6429830b0c9d70cf05415c
raysun93 <raysun931013@gmail.com>
2024-11-06 12:39:57 +1100

    delete wrong files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // 可以添加自定义颜色
+        primary: '#007AFF',
+      },
+    },
   },
   plugins: [],
-}
-
+};
