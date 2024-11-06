8ab65dffe027bf785b6cedfadd5dea7912b38331
raysun93 <raysun931013@gmail.com>
2024-11-06 20:47:20 +1100

    fix_bug

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,13 @@
+// src/pages/Home/Home.jsx
 import React from 'react';
 import HeroSection from '../../components/HeroSection/HeroSection';
-import HeroSection from '../../components/HeroSection/SillsSection';
+import SkillsSection from '../../components/SkillsSection/SkillsSection';
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <SkillsSection />
-      {/* 后续会添加更多部分 */}
     </div>
   );
 };
